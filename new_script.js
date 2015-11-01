4089e85361681b53b488554c89beeeb8a7d577df
Chase Gruber <chasegruber@gmail.com>
2015-11-01 18:40:04 -0500

    block out remaining components

--- new_script.js
+++ new_script.js
@@ -1,5 +1,3 @@
-// $(document).on('ready',function(){
-
 var mlb = {
   svgAtt:{
     width:parseFloat(d3.select('.eight.columns').style('width')),
@@ -18,6 +16,9 @@ var mlb = {
       .domain([this.svgAtt.margins.left,this.svgAtt.width-this.svgAtt.margins.right]);
     this.svgAtt.yScale = d3.scale.linear()
       .range([(this.svgAtt.height-this.svgAtt.margins.bottom),this.svgAtt.margins.top]);
+    // this.svgAtt.lineFx = d3.svg.line().interpolate('linear');
+    // this.page.xAxis = d3.svg.axis().orient('bottom').tickFormat(d3.format('d'));
+    // this.page.yAxis = d3.svg.axis().orient('left');
   },
   page:{
     hitters:{},
@@ -85,6 +86,56 @@ var mlb = {
     var lines = players.append('path')
       .style('fill','none').style('stroke-width',3).style('stroke-opacity',0.5).style('stroke','green');
   },
+  lineChange: function(field,set){
+    // var fieldMax = d3.max(hitData, function(d) {
+    //   return d3.max(d[set], function(e){
+    //     return e[field];
+    //   });
+    // });
+    // y.domain([0,fieldMax]);
+    // lineFx.defined(function(d) { return d[field] >= 0; }).y(function (d) { return y(d[field]) });
+    // lines.transition().duration(500).attr('d', function(d){return lineFx(d[set])});
+    // hitDiv.select('.yaxis').transition().duration(500).call(yAxis.scale(y));
+  },
+  timeChange:function(time){
+    //check if playerbutton is clicked, could refactor into own function returning player if clicked//
+    // var clicked = hitDiv.select('.playerBtn.clicked');
+    // if (clicked[0][0] == null){
+    //   if (time == 'career'){
+    //     var min = 1;
+    //     var max = d3.max(hitData, function(d){return d.seasonal.length});
+    //     lineFx.x(function(d,i){return x(i+1)});
+    //   }else{
+    //     var min = d3.min(hitData, function(d) {return d.seasonal[0].year;});
+    //     var max = d3.max(hitData, function(d) {return d.seasonal[d.seasonal.length-1].year;});
+    //     lineFx.x(function (d) { return x(d.year) })
+    //   }
+    // }else{
+    //   //if player button is clicked, make x axis fit just that player, should do the same with y axis//
+    //   var player = clicked.attr('player');
+    //   var min,max;
+    //   hitDiv.select('g.player.'+player).each(function(d){
+    //     if (time == 'career'){
+    //       min = 1;
+    //       max = d.seasonal.length;
+    //       lineFx.x(function(d,i){return x(i+1)});
+    //     }else{
+    //       min = d.seasonal[0].year;
+    //       max = d.seasonal[d.seasonal.length-1].year;
+    //       lineFx.x(function (d) { return x(d.year) })
+    //     }
+    //   })
+    // }
+    // x.domain([min,max]);
+    // xInv.range([min,max]);
+    //
+    // var field = hitDiv.select('#clicker').node().value;
+    // hitDiv.select('.xaxis').transition().duration(500).call(xAxis.scale(x));
+    //
+    // var set = hitDiv.select('input[name="optradio2"]:checked').node().value;
+    //
+    // lineChange(field,set);
+  },
   makeButtons(data,that,set){
     data.forEach(function(dp){
       var guy = dp.seasonal[0].player;
@@ -97,6 +148,56 @@ var mlb = {
       btn.append('p').text(name);
     })
   },
+  buttonHover: function(player){
+    hitDiv.selectAll('g.player').style('opacity',0.1);
+    hitDiv.selectAll('g.player.'+player).style('opacity',1);
+  },
+  chartReset: function(){
+    hitDiv.selectAll('.playerBtn').attr('class','playerBtn');
+    hitDiv.selectAll('g.player').style('opacity',1);
+  },
+  clickPlayer: function(player){
+    var time  = hitDiv.select('input[name="optradio1"]:checked').node().value;
+    timeChange(time)
+    hitSvg
+      .on('mouseover',toolHov).on('mousemove',toolHov).on('mouseout',toolUnhov);
+  },
+  // toolHov: function(){
+  //   var player = hitDiv.select('.playerBtn.clicked').attr('player');
+  //   var data = hitData.filter(function(b){return b.player == player})[0];
+  //   var field = hitDiv.select('#clicker').node().value;
+  //   var time = hitDiv.select('input[name="optradio1"]:checked').node().value;
+  //   var set  = hitDiv.select('input[name="optradio2"]:checked').node().value;
+  //
+  //   var left = d3.event.offsetX;
+  //   var year = d3.round(xInv(left),0);
+  //   var top = d3.event.offsetY;
+  //
+  //   var xLeft = x(year);
+  //   var yData = (time == 'time')?data[set].filter(function(b){return b.year == year})[0]:
+  //   data[set][year-1];
+  //
+  //   if (yData){
+  //     var yTop = y(yData[field]);
+  //     var mid = y.domain()[1]/2;
+  //     cTop = (yData[field]>mid)?100:-100;
+  //     //y2 = (yData[field]>mid)?100:-100;
+  //     if (yTop){
+  //       hitTip.style('visibility','visible')
+  //         .attr('transform','translate('+xLeft+','+yTop+')');
+  //       hitTip.selectAll('*').attr('transform','translate(0,'+cTop+')');
+  //       hitTip.select('line').attr('y1',0).attr('y2',cTop*(-1))
+  //       //hitTip.select('circle').attr('transform','translate(0,'+cTop+')');
+  //     }
+  //     //hitTip.select('line').attr('y1',yTop).attr('y2',y(yData[field]))
+  //   }else{
+  //     hitTip.style('visibility','hidden')
+  //   }
+  //   hitTip.select('text').text(d3.round(yData[field],3));
+  // },
+  // toolUnhov: function(){
+  //   hitTip.style('visibility','hidden');
+  // },
   calculateCumulative(data,that,set){
     data.forEach(function(dp){
       var cumulative = dp.cumulative;
@@ -162,12 +263,64 @@ var mlb = {
         }
       }
     })
+  },
+  bindEvents:function(){
+    // hitDiv.select('#clicker').on('change', function(){
+    //   var field = d3.select(this).node().value;
+    //   var set = hitDiv.select('input[name="optradio2"]:checked').node().value;
+    //   lineChange(field,set);
+    // });
+    //
+    // hitDiv.selectAll('input[name="optradio1"]').on('change', function () {
+    //     var time = d3.select(this).node().value;
+    //     timeChange(time);
+    // });
+    //
+    // hitDiv.selectAll('input[name="optradio2"]').on('change', function () {
+    //     var field = hitDiv.select('#clicker').node().value;
+    //     var set = d3.select(this).node().value;
+    //     lineChange(field,set);
+    // });
+    //
+    // hitDiv.selectAll('.playerBtn').on('mouseover', function(){
+    //   var player = d3.select(this).attr('player');
+    //   buttonHover(player);
+    // })
+    // .on('mousemove', function(){
+    //   var player = d3.select(this).attr('player');
+    //   buttonHover(player);
+    // })
+    // .on('mouseout', chartReset);
+    // hitDiv.selectAll('.playerBtn').on('click',function(){
+    //   var clickedClasses = d3.select(this).attr('class').split(" ");
+    //   //hitDiv.selectAll('g.player').on('mousemove',null).on('mouseover',null).on('mouseout',null);
+    //   hitSvg.on('mousemove',null).on('mouseover',null).on('mouseout',null);
+    //   if (clickedClasses.indexOf('clicked') !== -1){
+    //     hitDiv.selectAll('.playerBtn').attr('class','playerBtn')
+    //       .on('mouseover', function(){
+    //         var player = d3.select(this).attr('player');
+    //         buttonHover(player);
+    //       })
+    //       .on('mousemove', function(){
+    //         var player = d3.select(this).attr('player');
+    //         buttonHover(player);
+    //       })
+    //       .on('mouseout', chartReset);
+    //       hitTip.style('visibility','hidden');
+    //       var time  = hitDiv.select('input[name="optradio1"]:checked').node().value;
+    //       timeChange(time);
+    //   }else{
+    //     hitDiv.selectAll('.playerBtn').attr('class','playerBtn');
+    //     d3.select(this).attr('class','playerBtn clicked');
+    //     hitDiv.selectAll('.playerBtn').on('mousemove',null).on('mouseover',null).on('mouseout',null);
+    //     var player = d3.select(this).attr('player');
+    //     hitTip.style('visibility','hidden');
+    //     buttonHover(player);
+    //     clickPlayer(player);
+    //   }
+    // });
   }
 }
-mlb.getPageComponents();
-mlb.getData('hitters');
-mlb.getData('pitchers');
-
-
-
-// });
+//mlb.getPageComponents();
+// mlb.getData('hitters');
+// mlb.getData('pitchers');
